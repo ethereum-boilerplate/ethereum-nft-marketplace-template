564232ca1ce8c236d5e622b5b37215b5ff9244d4
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2022-01-18 13:40:11 +0100

    refactor(marketplace): more typescript

---
+++ src/hooks/useERC20.ts
@@ -0,0 +1,45 @@
+import {useChain, useMoralisWeb3Api, useWeb3ExecuteFunction} from "react-moralis";
+
+const approveAbi = {"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}
+
+const useERC20 = () => {
+
+    const { fetch: fetchApprove, data: approvalResult } = useWeb3ExecuteFunction()
+    const { account } = useMoralisWeb3Api()
+    const { chainId } = useChain()
+
+    const balance = async (contractAddress: string, userAddress: string): Promise<string> => {
+        const req = await  account.getTokenBalances({
+            address: userAddress,
+            ["chain" as any]: chainId,
+            token_addresses: [ contractAddress ]
+        })
+        return req[0].balance
+    }
+
+    const allowance = () => {
+
+    }
+
+    const approve = (contractAddress: string, spender: string, amount: string) => {
+        fetchApprove({
+            params:{
+                abi: [ approveAbi ],
+                contractAddress: contractAddress,
+                functionName: "approve",
+                params: {
+                    spender: spender,
+                    value: amount
+                }
+            }
+        }).then(() => {}).catch(() => {})
+    }
+
+    return {
+        approve,
+        approvalResult,
+        balance
+    }
+}
+
+export default  useERC20;
