90a5a5f3c4cea4aaab048680d67bc0f998febf3f
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2022-01-20 12:05:17 +0100

    fix(index): missing admin index

---
+++ src/components/Admin/index.ts
@@ -0,0 +1,6 @@
+export const ProjectChainId = "" // Project Chain Id
+export const AdminAddress = "" // CHANGE TO YOUR ADDRESS
+export const MasterKey = "" // MORALIS MASTERKEY (WILL BE REMOVED)
+export const StandardCurrency = "" // STANDARD CURRENCY FOR BUYING NFTS
+export const RegistryAddress = "" // Registry address
+export const DEFAULT_ADMIN_ROLE = "0x00" // DO NOT CHANGE
