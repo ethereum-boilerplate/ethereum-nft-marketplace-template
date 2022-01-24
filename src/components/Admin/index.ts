c227d533eaf811a7d05b65520d776f81f8577bcd
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2022-01-24 13:20:40 +0100

    refactor(all): web3ui/typescript

--- src/components/Admin/index.ts
+++ src/components/Admin/index.ts
@@ -1,6 +1,3 @@
-export const ProjectChainId = "" // Project Chain Id
-export const AdminAddress = "" // CHANGE TO YOUR ADDRESS
+export const ProjectChainId = "0x13881" // Project Chain Id
 export const MasterKey = "" // MORALIS MASTERKEY (WILL BE REMOVED)
-export const StandardCurrency = "" // STANDARD CURRENCY FOR BUYING NFTS
-export const RegistryAddress = "" // Registry address
-export const DEFAULT_ADMIN_ROLE = "0x00" // DO NOT CHANGE
+export const RegistryAddress = "0x431F3E7c03FdE227DCaE1a61Ba9CD93a1724747B" // Registry address
