f22c00b81c47f182d461ff8bf410e6775778d3ee
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2022-01-27 13:09:15 +0100

    refactor(cards): polish ui

--- src/views/Admin/Module/Adder/components/ModuleSelector.tsx
+++ src/views/Admin/Module/Adder/components/ModuleSelector.tsx
@@ -1,9 +1,17 @@
-// @ts-nocheck
-import { Card, Illustration } from 'web3uikit';
+import {Card, Illustration, Tooltip} from 'web3uikit';
 import { Typography } from 'uikit/Typography';
 import { ModulesGridStyled } from '../Adder.styles';
-import { dropModule, packModule, marketplaceModule, tokenModule, collectionModule, bundleModule } from '../../types/modules';
+import {
+    dropModule,
+    packModule,
+    marketplaceModule,
+    tokenModule,
+    collectionModule,
+    bundleModule,
+    moduleTypes, IModule
+} from '../../types/modules';
 import { useHistory, useRouteMatch } from 'react-router';
+import {ReactNode} from "react";
 
 const ModuleSelector = () => {
     const nftModules = [collectionModule, bundleModule, dropModule];
@@ -17,28 +25,52 @@ const ModuleSelector = () => {
         push(`${path}/${target}`);
     };
 
+    const isComingSoon = (module: moduleTypes): boolean => {
+        return ((module === "bundleModule") || (module === "lazyMintModule") || (module === "packModule"))
+    }
+
+    const printCard = (module: IModule): ReactNode[] => {
+        return (
+            [
+                <Card
+                title={module.title}
+                tooltipText={module.desc}
+                children={[
+                    <div
+                        key={module.title}
+                        style={{
+                            display: 'grid',
+                            placeItems: 'center',
+                        }}
+                    >
+                        <Illustration width="130px" height="200px" logo={module.logo} />
+                    </div>,
+                ]}
+                // @ts-ignore
+                isDisabled={isComingSoon(module.key)}
+                key={module.key}
+             />
+            ]
+        )
+    }
+
     return (
         <>
             <Typography variant="h4">NFT Modules</Typography>
             <ModulesGridStyled>
                 {nftModules.map((module) => {
                     return (
-                        <div key={module.title} onClick={() => pushToHistory(module.key)} style={{ cursor: 'pointer' }}>
-                            <Card
-                                title={module.title}
-                                tooltipText={module.desc}
-                                children={[
-                                    <div
-                                        key={module.title}
-                                        style={{
-                                            display: 'grid',
-                                            placeItems: 'center',
-                                        }}
-                                    >
-                                        <Illustration width="130px" height="200px" logo={module.logo} />
-                                    </div>,
-                                ]}
-                            />
+                        <div key={module.title} onClick={() => {
+                            if(isComingSoon(module.key)) return;
+                            pushToHistory(module.key)
+                        }} style={{ cursor: isComingSoon(module.key) ? 'not-allowed' : 'pointer' }}>
+                            { isComingSoon(module.key) ? printCard(module) : (
+                                <Tooltip
+                                    position={"bottom"}
+                                    text={"Coming Soon 👀"}
+                                    children={printCard(module)}
+                                />
+                            ) }
                         </div>
                     );
                 })}
@@ -47,22 +79,17 @@ const ModuleSelector = () => {
             <ModulesGridStyled>
                 {otherModules.map((module) => {
                     return (
-                        <div key={module.title} onClick={() => pushToHistory(module.key)} style={{ cursor: 'pointer' }}>
-                            <Card
-                                title={module.title}
-                                tooltipText={module.desc}
-                                children={[
-                                    <div
-                                        key={module.title}
-                                        style={{
-                                            display: 'grid',
-                                            placeItems: 'center',
-                                        }}
-                                    >
-                                        <Illustration width="130px" height="200px" logo={module.logo} />
-                                    </div>,
-                                ]}
-                            />
+                        <div key={module.title} onClick={() => {
+                            if(isComingSoon(module.key)) return;
+                            pushToHistory(module.key)
+                        }} style={{ cursor: isComingSoon(module.key) ? 'not-allowed' : 'pointer' }}>
+                            { isComingSoon(module.key) ? printCard(module) : (
+                                <Tooltip
+                                    position={"top"}
+                                    text={"Coming Soon 👀"}
+                                    children={printCard(module)}
+                                />
+                            ) }
                         </div>
                     );
                 })}
